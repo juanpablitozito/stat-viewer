cbe4e54bda6e9cc18d534814c4b220f339744293
JuanMartins <juanmartinsr07@gmail.com>
2025-04-25 16:16:08 -0300

    carrossel

---
+++ assets/script.js
@@ -0,0 +1,19 @@
+let indice = 0;
+  const slides = document.getElementById("slides");
+  const total = slides.children.length;
+
+  function atualizarSlide() {
+    slides.style.transform = `translateX(-${indice * 100}%)`;
+  }
+
+  function avancarSlide() {
+    indice = (indice + 1) % total;
+    atualizarSlide();
+  }
+
+  function voltarSlide() {
+    indice = (indice - 1 + total) % total;
+    atualizarSlide();
+  }
+
+  setInterval(avancarSlide, 6500);
